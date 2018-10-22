02202cee195e85481654ff2c25fbd493e32647a9
Tao Luo <tluo1996@uchicago.edu>
2018-10-22 12:06:14 -0500

    new

---
+++ fb/535EncodeandDecodeTinyURL.java
@@ -0,0 +1,13 @@
+public class Codec {
+    HashMap<Integer, String> map = new HashMap<Integer, String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        map.put(longUrl.hashCode(), longUrl);
+        return "http://tinyurl.com/" + longUrl.hashCode();
+    }
+    
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }
+}
