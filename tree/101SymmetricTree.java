57dcee10bad071ae0b722c83c1886ecf1a651521
Tao Luo <tluo1996@uchicago.edu>
2018-11-21 09:17:00 -0600

    new

--- tree/101SymmetricTree.java
+++ tree/101SymmetricTree.java
@@ -8,18 +8,42 @@
  * }
  */
 class Solution {
+    /*
+     public boolean isSymmetric(TreeNode root) {
+     if(root == null)
+     return true;
+     return dfs(root.left, root.right);
+     }
+     private boolean dfs(TreeNode left, TreeNode right){
+     if(left == null || right == null)
+     return left==null && right == null;
+     return left.val == right.val && dfs(left.right, right.left) && dfs(left.left, right.right);
+     }
+     */
     public boolean isSymmetric(TreeNode root) {
-        if(root == null){
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        if(root == null)
             return true;
+        q.offer(root);
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode p1 = q.poll();
+            TreeNode p2 = q.poll();
+            if(p1.val != p2.val)
+                return false;
+            if( (p1.left == null) != (p2.right == null))
+                return false;
+            if(p1.left != null){
+                q.offer(p1.left);
+                q.offer(p2.right);
+            }
+            if( (p1.right == null) != (p2.left == null))
+                return false;
+            if(p1.right != null){
+                q.offer(p1.right);
+                q.offer(p2.left);
+            }
         }
-        return dfs(root.left, root.right);
-    }
-    
-    private boolean dfs(TreeNode left, TreeNode right){
-        if(left == null || right == null)
-            return left == null && right == null;
-        else{
-            return left.val == right.val && dfs(left.left, right.right) && dfs(left.right, right.left);
-        }
+        return true;
     }
 }
